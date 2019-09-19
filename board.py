561e0274ce37dc5fed1bd3e0c3d13bae02f79cd3
Sebastian Mangelsen <sebastian.mangelsen@volvocars.com>
2019-09-19 22:27:39 +0200

    provides a simplified menu
    - adds statistics for moves

--- board.py
+++ board.py
@@ -7,32 +7,40 @@ class Board(object):
     def __init__(self):
         self.board = None
         self._init_board()
+        self.moves = 0
 
     def _init_board(self):
-        if not self.board:
-            _board = [[Rock('w'), Knight('w'), Bishop('w'), Queen('w'), King('w'), Bishop('w'), Knight('w'), Rock('w')],
-                      [Pawn('w') for x in range(8)], [None for x in range(8)], [None for x in range(8)],
-                      [None for x in range(8)], [None for x in range(8)], [Pawn('b') for x in range(8)],
-                      [Rock('b'), Knight('b'), Bishop('b'), Queen('b'), King('b'), Bishop('b'), Knight('b'), Rock('b')]]
-            self.board = _board.copy()
+        self.moves = 0
+        _board = [[Rock('w'), Knight('w'), Bishop('w'), Queen('w'), King('w'), Bishop('w'), Knight('w'), Rock('w')],
+                  [Pawn('w') for x in range(8)], [None for x in range(8)], [None for x in range(8)],
+                  [None for x in range(8)], [None for x in range(8)], [Pawn('b') for x in range(8)],
+                  [Rock('b'), Knight('b'), Bishop('b'), Queen('b'), King('b'), Bishop('b'), Knight('b'), Rock('b')]]
+        self.board = _board.copy()
 
     def __str__(self):
-        _str = "this is a board\n"
         _row = len(self.board)
-        _str += "   A  B  C  D  E  F  G  H \n"
+        _str = ""
+        _str = "stats: %d moves\n" % self.moves
+        _str += "     A   B   C   D   E   F   G   H \n\n"
         for row in self.board:
-            _str += "%d" % _row
+            _str += "%d  " % _row
             _row -= 1
             for tile in row:
                 if tile:
-                    _str += " %-2s" % tile
+                    _str += " %03s" % tile
                 else:
-                    _str += "  x"
-
+                    _str += " %03s" % 'x'
             _str += '\n'
         return _str
 
     def _init_from_file(self):
         pass
+
+    def move(self, start, to):
+        print('move from %s to %s' % (start, to))
+        self.moves += 1
+
+    def init(self):
+        self._init_board()
 
 
--- chessgame.py
+++ chessgame.py
@@ -1,12 +1,42 @@
+import os
 
 from board import Board
+
+commands = {"m": "move", "i": "init", "q": "quit", "s": "start"}
+
+
+def print_cmds():
+    for command in commands.keys():
+        print('%s => %s' % (command, commands[command]))
+
+
+def cmd_is_valid(key):
+    if key in commands.keys():
+        return 1
+    else:
+        return 0
 
 
 def main():
 
     b = Board()
 
-    print(b)
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(b)
+        print_cmds()
+        _c = input('next command: ')
+        if cmd_is_valid(_c):
+            print("received: %s" % commands[_c])
+            if _c == 'q':
+                break
+            elif _c == 'm':
+                _from = input('start: ')
+                _to = input('from: ')
+                b.move(_from, _to)
+        else:
+            print('received invalid command')
+            break
 
 
 if __name__ == '__main__':
