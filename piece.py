a7b977853d200f8e0f29e6bc7a3c240264159b2c
Sebastian Mangelsen <sebastian.mangelsen@volvocars.com>
2019-09-19 23:41:46 +0200

    adding vector for moving pieces

--- piece.py
+++ piece.py
@@ -14,10 +14,11 @@ class Color(Enum):
 
 
 class Piece(object):
+    """implements a base class for pieces in a chess game"""
 
     PIECE_TYPE_MAP = {
         'K': 'King',
-        'q': 'Queen',
+        'Q': 'Queen',
         'r': 'Rock',
         'k': 'Knight',
         'b': 'Bishop',
@@ -26,6 +27,7 @@ class Piece(object):
 
     def __init__(self, color):
         self.name = None
+        self.moves = None
         if color in ['w', 'b']:
             self.color = Color.WHITE if color == 'w' else Color.BLACK
         else:
@@ -41,45 +43,91 @@ class Piece(object):
 
 
 class Knight(Piece):
+    """Implement specialization for Knights"""
 
     def __init__(self, color):
         super(Knight, self).__init__(color)
         self.name = 'k'
+        self.moves = {
+            "directions": [[-2, -1], [-2, 1],
+                           [-1, -2], [-1, 2],
+                           [1, -2], [1, 2],
+                           [2, -1], [2, 1]],
+            "steps": 1,
+            "jump": True
+        }
 
 
 class King(Piece):
+    """Implement specialization for a king"""
     
     def __init__(self, color):
         super(King, self).__init__(color)
         self.name = 'K'
+        self.moves = {
+            "directions":  [[-1, -1], [-1, 1], [-1, 0],
+                    [1, -1], [1, 1], [1, 0],
+                    [0, 1], [0, -1]],
+            "steps": None,
+            "jump": False
+        }
 
 
 class Queen(Piece):
+    """Implement specialization for a queen"""
 
     def __init__(self, color):
         super(Queen, self).__init__(color)
-        self.name = 'q'
+        self.name = 'Q'
+        self.moves = {
+            "directions":  [[-1, -1], [-1, 1], [-1, 0],
+                    [1, -1], [1, 1], [1, 0],
+                    [0, 1], [0, -1]],
+            "steps": None,
+            "jump": False
+        }
 
 
 class Rock(Piece):
+    """Implement specialization for a rock"""
 
     def __init__(self, color):
         super(Rock, self).__init__(color)
         self.name = 'r'
+        self.moves = {
+            "directions": [[-1, 0], [0, 1], [1, 0], [0, -1]],
+            "steps": None,
+            "jump": False
+        }
 
 
 class Bishop(Piece):
-
+    """Implement specialization for a bishop"""
     def __init__(self, color):
         super(Bishop, self).__init__(color)
         self.name = 'b'
 
+        self.moves = {
+            "directions": [[-1, 1], [1, 1], [1, -1], [-1 ,-1]],
+            "steps": None,
+            "jump": False
+        }
+
 
 class Pawn(Piece):
+    """Implement specialization for a pawn"""
 
     def __init__(self, color):
         super(Pawn, self).__init__(color)
         self.name = 'p'
+        self.moves = {
+            "directions":  [[-1, -1], [-1, 1], [-1, 0],
+                            [1, -1], [1, 1], [1, 0],
+                            [0, 1], [0, -1]],
+            "steps": 1,
+            "steps_start": 2,
+            "jump": False
+        }
 
 
 
